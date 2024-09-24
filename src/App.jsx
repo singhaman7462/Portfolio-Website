3166c658bbb8442b6bf1efa9edfa165f1dfb546c
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2024-09-24 20:01:50 +0530

    Completed Experience section of the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar/Navbar'
 import styles from'./App.module.css'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import Experience from './components/Experience/Experience'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   )
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import skills from "../../data/skills.json"
+import { getImageUrl } from '../../utils'
+import history from "../../data/history.json"
+import styles from "./Experience.module.css"
+function Experience() {
+  return (
+   <section id="experience" className={styles.container}>
+       <h2 className={styles.title}>Experience</h2>
+       <div className={styles.content}>
+        <div className={styles.skills}>
+ {
+    skills.map((skill,id)=>{
+       return <div key={id} className={styles.skill}>
+          <div className={styles.skillImageContainer}>
+            <img src={getImageUrl(skill.imageSrc)} alt={skill.title}/>
+          </div>
+          <p>
+            {skill.title}
+          </p>
+        </div>
+    }
+)
+ }
+        </div>
+        <ul className={styles.history}>
+{
+    history.map((historyItem,id)=>{
+        return <li key={id} className={styles.historyItem}>
+            <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} height="75px" width="75px" />
+            <div className={styles.historyItemDetails}>
+                <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                <p>{`${historyItem.startDate} to
+                ${historyItem.endDate}`}</p>
+                <ul>
+                    {historyItem.experiences.map((exp,id)=>{
+                        return <li key={id}>
+                          {exp}
+                        </li>
+                    })}
+                </ul>
+            </div>
+        </li>
+    })
+}
+        </ul>
+       </div>
+   </section>
+  )
+}
+
+export default Experience
