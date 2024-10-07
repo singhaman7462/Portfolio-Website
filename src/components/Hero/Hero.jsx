f791f5126fa685f20ccc57b7847b84f60a0b0586
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2024-10-07 07:24:06 +0530

    Hid the menu button in responsive website and updated the profile photo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,12 +5,12 @@ function Hero() {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi I am Aman</h1>
-        <p className={styles.description}>I am a MERN Stack Developer.Reach out if you'd like to learn more.</p>
+        <h1 className={styles.title}>Hi I am Aman Kumar Singh</h1>
+        <p className={styles.description}>I am a MERN Stack Developer and an aspiring Software Developer.Reach out if you'd like to learn more.</p>
         <a className={styles.contactBtn} href="mailto:singh03aman2103@gmail.com">Contact Me</a>
 
       </div>
-      <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="My Image"></img>
+      <img className={styles.heroImg} src={getImageUrl("hero/myImage.jpg")} alt="My Image"></img>
       <div className={styles.topBlur}></div>
       <div className={styles.bottomBlur}></div>
     </section>
